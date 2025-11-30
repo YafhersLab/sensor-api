0af0890309e7aa7065f93e1c068bafe393d0370a
YafhersLab <yamc1605@gmail.com>
2025-11-30 12:38:00 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// Estado de cada "campo"
+let canales = {
+  TempAmb: "0",
+  HumAmb: "0",
+  HumSuelo: "0",
+  Battery: "0"
+};
+
+// Leer el valor de un canal
+app.get("/canal/:nombre", (req, res) => {
+  const nombre = req.params.nombre;
+  if (canales[nombre] !== undefined) {
+    res.send(canales[nombre].toString());
+  } else {
+    res.status(404).send("Canal no existe");
+  }
+});
+
+// Escribir un valor en un canal
+app.post("/canal/:nombre", (req, res) => {
+  const nombre = req.params.nombre;
+  const valor = req.body.valor;
+  if (canales[nombre] !== undefined) {
+    canales[nombre] = valor.toString();
+    res.send("OK");
+  } else {
+    res.status(400).send("Canal no existe");
+  }
+});
+
+// Actualizar varios campos
+app.post("/canales", (req, res) => {
+  const data = req.body; // JSON con varios campos
+  for (const key in data) {
+    if (canales[key] !== undefined) {
+      canales[key] = data[key].toString();;
+    }
+  }
+  res.send("OK");
+});
+
+// Leer varios canales a la vez
+app.get("/canales", (req, res) => {
+  res.json({
+    TempAmb: canales.TempAmb.toString(),
+    HumAmb: canales.HumAmb.toString(),
+    HumSuelo: canales.HumSuelo.toString(),
+    Battery: canales.Battery.toString()
+  });
+});
+
+// Servidor en puerto 3000
+app.listen(3000, () => console.log("Servidor activo"));
